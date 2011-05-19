f9de6c8e5df3052a9cebc643a4c6467af415c5aa
cho45 <cho45@lowreal.net>
2011-05-19 11:08:08 +0900

    init

---
+++ lib/Plack/App/CocProxy.pm
@@ -0,0 +1,36 @@
+package Plack::App::CocProxy;
+
+use strict;
+use warnings;
+
+our $VERSION = '0.01';
+
+
+1;
+__END__
+
+=head1 NAME
+
+Plack::App::CocProxy - 
+
+=head1 SYNOPSIS
+
+  use Plack::App::CocProxy;
+
+
+=head1 DESCRIPTION
+
+Plack::App::CocProxy is 
+
+=head1 AUTHOR
+
+cho45 E<lt>cho45@lowreal.netE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
